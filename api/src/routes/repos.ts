0d1a37398241e83f40072f40703ebca96ee1b894
A <@gmail.com>
2022-05-10 01:00:35 -0500

    feat: create api endpoint that aggregate data from github and local file repos.json

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,20 @@
 import { Router, Request, Response } from 'express';
 
 export const repos = Router();
+import axios from 'axios';
+import { readFile } from 'fs/promises';
 
 repos.get('/', async (_: Request, res: Response) => {
-  res.header('Cache-Control', 'no-store');
+  const url = 'https://api.github.com/users/silverorange/repos';
+  const file = `${__dirname}/../../data/repos.json`;
+  const reposGit = await axios(url).then(({ data }) => data);
+  const reposLocal = await readFile(file, 'utf8').then((data) =>
+    JSON.parse(data)
+  );
 
-  res.status(200);
+  const repos = [...reposGit, ...reposLocal].filter((repo) => !repo.fork);
+  res.header('Cache-Control', 'no-store');
+  res.status(200).json(repos);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
 });
